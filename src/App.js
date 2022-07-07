9fc1729d462813ce863e78e6edca3692fb5fb487
MNATorres <mtri21019@gmail.com>
2022-07-07 12:05:13 -0300

    se agrega funcion de eliminar

--- src/App.js
+++ src/App.js
@@ -7,12 +7,22 @@ function App() {
   const [list, setList] = useState([])
 
   let actualizar = (e) => {
-    setText(e.currenTarjet.value)
+    setText(e.currentTarget.value)
   }
 
   let agregar = () => {
-    setList([...list, {title: text, value: false}])
+    setList([...list, {title: text, value: true}])
     setText("")
+  }
+
+  let eliminar = (i) => {
+    setList(list.filter((_, j) =>{
+      return i != j
+    }))
+  }
+
+  let reset = () => {
+    setList([])
   }
 
   return (
@@ -24,11 +34,17 @@ function App() {
         type="text" 
         placeholder="Escriba aquí.." />
         <button onClick={agregar}>Añadir</button>
-        <button>Reset</button>
+        <button onClick={reset}>Reset</button>
       </form>
       <ul>
-        {list.map((elemento) => {
-          return <li>{elemento.title}</li>
+        {list.map((elemento, t) => {
+          return (
+          <li key={t}>
+            <input type='checkbox'/>
+            {elemento.title}
+            <button onClick={() => eliminar(t)}>x</button>
+          </li>
+          )
         } )}
       </ul>
     </div>
