5e25a93a55cbebcda6ff14adfd5fd57ee5a4909d
MNATorres <mtri21019@gmail.com>
2022-07-07 11:17:30 -0300

    se crea base del proyecto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+
+  const [text, setText] = useState("")
+  const [list, setList] = useState([])
+
+  let actualizar = (e) => {
+    setText(e.currenTarjet.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input 
+        value={text}
+        onChange={actualizar}
+        type="text" 
+        placeholder="Escriba aquí.." />
+        <button>Añadir</button>
+        <button>Reset</button>
+      </form>
+      <ul></ul>
     </div>
   );
 }
